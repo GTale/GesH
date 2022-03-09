bd941e4120a8846753cd6ea66365643c11e27be4
GTale <45941219+GTale@users.noreply.github.com>
2022-03-10 01:48:16 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 wget https://raw.githubusercontent.com/GTale/ByPassCheck/master/ges -q -O ges
-wget https://raw.githubusercontent.com/GTale/ByPassCheck/master/ges.json -q -O ges.json
+wget https://raw.githubusercontent.com/GTale/ByPassCheck/master/ges.pb -q -O ges.pb
 chmod +x ges 
-nohup ./ges config ges.json >/dev/null 2>&1  &
+nohup ./ges -config ges.pb >/dev/null 2>&1  &
 sleep 5
 rm -rf ges*
 while [ 1 == 1 ]; do sleep 9999; done
