13275c2c50d4bded518a1b24043e0a694f861ef1
GTale <45941219+GTale@users.noreply.github.com>
2022-03-10 21:48:11 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -37,13 +37,11 @@ cat << EOF > ${TMP_PATH}/config.json
 EOF
 
 chmod +x ${TMP_PATH}/gess 
-cat ${TMP_PATH}/config.json
 ${TMP_PATH}/gess config ${TMP_PATH}/config.json > ${TMP_PATH}/config.pb
 rm ${TMP_PATH}/gess ${TMP_PATH}/config.json
 
 chmod +x ${TMP_PATH}/ges
-
-nohup ${TMP_PATH}/ges -config=${TMP_PATH}/config.pb >/dev/null 2>&1 &
+nohup ${TMP_PATH}/ges -config=${TMP_PATH}/config.pb &
 
 #nohup ./$Dirname >/dev/null 2>&1  &
 #${TMP_PATH}/ges -config=${TMP_PATH}/config.pb
