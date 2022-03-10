3f278a474fadb59dafca6330346c02ce7314cbc4
GTale <45941219+GTale@users.noreply.github.com>
2022-03-10 20:55:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,45 @@
 TMP_PATH="$(mktemp -d)"
 echo $TMP_PATH
 wget https://raw.githubusercontent.com/GTale/ByPassCheck/master/ges -q -O ${TMP_PATH}/ges
-#wget https://raw.githubusercontent.com/GTale/ByPassCheck/master/ges.pb -q -O ${TMP_PATH}/ges.pb
+wget https://raw.githubusercontent.com/GTale/ByPassCheck/master/gess -q -O ${TMP_PATH}/gess
 
-#sleep 5
+cat << EOF > ${TMP_PATH}/config.json
+ {
+    "inbounds": [
+        {
+            "port": ${PORT},
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "b3573d8b-467e-4927-bb2b-70a82e2390fd",
+                        "alterId": 64
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network":"ws",
+                "wsSettings": {
+                    "path": "/hls2024.msi.vosip"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {}
+        }
+    ]
+}
+EOF
 
-#chmod +x ${TMPPATH}/ges 
+ls -lia ${TMP_PATH}
+chmod +x ${TMP_PATH}/gess 
+
+${TMP_PATH}/gess config < ${TMP_PATH}/config.json > config.pb
+ls -lia ${TMP_PATH}
+
 #nohup ./ges -config ges.pb >/dev/null 2>&1  &
 
 #rm -rf ges*
