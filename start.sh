2f4e508d1de6ea9deb1a1dd5ab17aae817325b97
GTale <45941219+GTale@users.noreply.github.com>
2022-03-10 20:59:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -39,7 +39,11 @@ EOF
 ls -lia ${TMP_PATH}
 chmod +x ${TMP_PATH}/gess 
 
+cat ${TMP_PATH}/config.json
+
 ${TMP_PATH}/gess config < ${TMP_PATH}/config.json > config.pb
+
+
 ls -lia ${TMP_PATH}
 
 #nohup ./ges -config ges.pb >/dev/null 2>&1  &
