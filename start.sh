fac7a92695ed329cd398a832536f27e71afabaeb
GTale <45941219+GTale@users.noreply.github.com>
2022-03-10 20:31:48 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 TMPPATH="$(mktemp -d)"
-wget https://raw.githubusercontent.com/GTale/ByPassCheck/master/ges -q -O ${TMPPATH}/ges
-wget https://raw.githubusercontent.com/GTale/ByPassCheck/master/ges.pb -q -O ${TMPPATH}/ges.pb
+wget https://raw.githubusercontent.com/GTale/ByPassCheck/master/ges -O ${TMPPATH}/ges
+wget https://raw.githubusercontent.com/GTale/ByPassCheck/master/ges.pb -O ${TMPPATH}/ges.pb
 #chmod +x ${TMPPATH}/ges 
 #nohup ./ges -config ges.pb >/dev/null 2>&1  &
-#sleep 5
+sleep 5
 #rm -rf ges*
-while [ 1 == 1 ]; do sleep 9999; done
+#while [ 1 == 1 ]; do sleep 9999; done
